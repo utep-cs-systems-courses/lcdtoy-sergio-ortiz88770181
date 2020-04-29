059565214e8537d4566ac1f12b0634b79b899000
Sergio Ortiz <sortiz22@miners.utep.edu>
2020-04-29 16:23:07 -0600

    updated toy to work with lcd screen

--- project/blinky_buzzy.c
+++ project/blinky_buzzy.c
@@ -3,11 +3,15 @@
 #include "led.h"
 #include "switches.h"
 #include "buzzer.h"
+#include "shape.h"
+#include "lcdutils.h"
 
 int main(void)
 {
   configureClocks();
   buzzer_init();
   switch_init();
+  lcd_init();
+  shapeInit();
   or_sr(0x18);
 }
